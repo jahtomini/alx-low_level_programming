0a6bb3a73a4dd5f9fa55ba3f10cdbbf1369509aa
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-17 03:51:22 +0100

    fix 'hello, printf'

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description - put some text
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
 }
