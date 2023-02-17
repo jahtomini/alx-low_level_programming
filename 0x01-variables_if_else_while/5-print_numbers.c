514078eb25d9c6e7186b908e02ed38442d8544c9
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-17 23:44:46 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* description - print the alphabet with putchar
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+
+	printf("\n");
+	return (0);
+}
