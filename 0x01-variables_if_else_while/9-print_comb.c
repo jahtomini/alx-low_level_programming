176af0e8229fae3ad2586ba8e50bcc901b68e22e
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-18 00:28:42 +0100

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ int main(void)
 	int c = 0;
 
 	while (c < 10)
+	{
 		putchar(48 + c);
 		if (c != 9)
+		{
 			putchar(',');
 			putchar(' ');
+		}
 		c++;
-
+	}
 	putchar('\n');
 	return (0);
 }
