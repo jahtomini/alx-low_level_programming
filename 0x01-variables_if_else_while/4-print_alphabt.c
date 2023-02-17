f9673e663c344c74d3d87b1b4f46555328833bb1
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-17 23:44:32 +0100

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* description - print the alphabet with putchar
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	for (d = 'A'; d <= 'Z'; d++)
+		if (d != 'Q' && d != 'E')		
+			putchar(d);
+
+	return (0);
+}
