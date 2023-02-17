c328f47350de7f76f18e3f0af4178b64d99c9399
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-17 22:55:32 +0100

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* description - print the alphabet with putchar
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+	char d;
+	for (c = 'a'; c != 'z'; c++)
+		putchar(c);
+	for (d = 'A'; d != 'Z'; d++)
+		putchar(d);
+	
+	putchar('\n');
+}
