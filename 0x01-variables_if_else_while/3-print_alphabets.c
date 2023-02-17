0e5bc9dba6c16bd871718629d46b3be653519c7b
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-17 23:01:22 +0100

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,9 +13,9 @@ int main(void)
 {
 	char c;
 	char d;
-	for (c = 'a'; c != 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-	for (d = 'A'; d != 'Z'; d++)
+	for (d = 'A'; d <= 'Z'; d++)
 		putchar(d);
 	
 	putchar('\n');
