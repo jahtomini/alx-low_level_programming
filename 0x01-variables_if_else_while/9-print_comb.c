25ceef69aaa4b2b501f321714103c6ad2ed80c85
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-18 00:25:33 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+		putchar(48 + c);
+		if (c != 9)
+			putchar(',');
+			putchar(' ');
+		c++;
+
+	putchar('\n');
+	return (0);
+}
