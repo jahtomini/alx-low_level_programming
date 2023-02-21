dd9791e6ac75c70199ab9c313657fbc070df2fc1
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-21 23:31:06 +0100

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* description - Print _putchar, followed by a new line
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c[9] = "_putchar\n";
+	for (int i; i < 9; i++)
+	{
+		_putchar(c[i]);
+
+	return (0)
+}
