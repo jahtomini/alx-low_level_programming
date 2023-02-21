037c160c3d29d682d6e2d0f05fd0aab9c98fcaf7
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-22 00:15:45 +0100

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* description - Print the alphabet in lowercase followed by a new line
+* print_alphabet - Entry point
+*
+* Description - Print the alphabet in lowercase followed by a new line
+*
 * Return: Always 0 (Success)
 */
 
@@ -12,9 +14,10 @@ void print_alphabet(void)
 	char c;
 	char n = '\n';
 
-	for (c = 'a'; c < 'z'; i++)
+	for (c = 'a'; c < 'z'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar(n);
+	return (0);
 }
