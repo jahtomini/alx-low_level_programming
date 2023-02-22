509f3221f82c88be3e29c75c05906998f78fb735
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-22 03:02:10 +0100

    task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add function
+ * @a: first
+ * @b: second
+ *
+ * Description: adds two numbers
+ * Return: result of the sum
+ */
+int add(int a, int b)
+{
+	int n = a + b;
+	return (n);
+}
