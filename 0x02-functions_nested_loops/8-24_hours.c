2c3bdccb07f798dba8ea8c4c0f329d2adbe1e4df
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-22 03:01:51 +0100

    task 9

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+void print_time(int hours, int min);
+
+/**
+ * jack_bauer -  function that prints every minute of the day
+ * of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Description: This is a function that prints time.
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			print_time(i, j);
+		}
+	}
+
+}
+
+
+
+/**
+ * print_time - function prints time format
+ * @hours: time hours
+ * @min: time minutes
+ *
+ * Description: util function.
+ * Returns: void
+*/
+
+void print_time(int hours, int min)
+{
+	if (hours < 10)
+	{
+		_putchar('0');
+		_putchar('0' + hours);
+	}
+	else
+	{
+		_putchar('0' + hours / 10);
+		_putchar('0' + hours % 10);
+	}
+
+	_putchar(':');
+
+	if (min < 10)
+	{
+		_putchar('0');
+		_putchar('0' + min);
+	}
+	else
+	{
+		_putchar('0' + min / 10);
+		_putchar('0' + min % 10);
+	}
+	_putchar('\n');
+}
