e27906a3e86f7ca72942c6ba46f8df297bfe8386
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-23 23:48:40 +0100

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul - int a * int b
+ * @a: input
+ * @b: input
+ *
+ * Description: multiplies two integers
+ * Return: the product
+ */
+
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	return (c);
+}
