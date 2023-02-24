6f34d26c03b8bce083772cac1be168dc4f6dda4a
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-24 02:03:04 +0100

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,19 +4,17 @@
  * print_numbers - print_numbers
  *
  * Description: print numbers from 0 to 9
- * Return: Always 0
+ * Return: No return value
  */
 
 void print_numbers(void)
 {
-	int i;
+	char i = 0;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
 	}
 
 	_putchar('\n');
-
-	return (0);
 }
