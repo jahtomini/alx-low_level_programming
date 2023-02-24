0f8cf6d2c0526d1a1989815b283aba27d5eaab83
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-24 03:40:11 +0100

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,22 @@
 void more_numbers(void)
 {
 	int i = 0;
-	char j = 0;
+	int j = 0;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = '0'; j <= '14'; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
-		}
-	}
+			
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			} 
 
-	_putchar('\n');
+			_putchar((j % 10) + '0');
+		}
+
+		_putchar('\n');
+
+	}
 }
