a8396827f6d907123dc4eb903675610a665e832f
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-02-28 01:20:29 +0100

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * puts - puts
+  * Description - print every other char in a string
+  * @str: the string being pointed at
+  *
+  * Return: empty
+  *
+  */
+
+
+void puts2(char *str)
+{
+	int len = strlen(str);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
