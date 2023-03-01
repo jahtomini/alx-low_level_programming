2fa07fcd89cd69faab3be78ed2efa960376a0f4d
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-03-01 19:46:54 +0100

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
   * *_strncpy - *_strncpy
@@ -15,9 +14,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *i;
+	int i;
 
-	i = strncpy(dest, src, n);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
