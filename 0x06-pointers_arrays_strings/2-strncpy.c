d1b1d7ec3c61bcbcab4413fcb2eea0966a995faa
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-03-01 19:41:36 +0100

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
   * *_strncpy - *_strncpy
@@ -6,19 +7,17 @@
   * @src: the string to be copied
   * @dest: the string that src would be replaced
   *
-  * @n: number of bytes copied from src 
-  * Return: empty
+  * @n: number of bytes copied from src
+  * Return: the string being copied onto
   *
   */
 
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-		for ( ; i < n; i++)
-			dest[i] = '\0';
+	char *i;
+
+	i = strncpy(dest, src, n);
 
 	return (dest);
 }
