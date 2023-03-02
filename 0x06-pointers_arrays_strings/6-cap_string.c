02cadfa4e7d8b2e39b356ce241495abfa64896fc
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-03-02 01:11:33 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * cap_string - Capitalize all letters in a string
+  * @s: the string
+  *
+  * Return: the capitalized string
+  *
+  */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		char j = s[i - 1];
+		if (j == ',' || j == ' ' || j == ';' || j == '.' || j == '!' || j == '?' || j == '"' || j == '(' || j == ')' || j == '{' || j == '}')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+
+	return (s);
+}
