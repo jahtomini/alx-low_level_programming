6dd7f021120917730f8e1ffd53d288f9dc166929
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-03-02 02:32:05 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,35 @@
-#include "main.h"
-
 /**
-  * cap_string - Capitalize all letters in a string
-  * @s: the string
-  *
-  * Return: the capitalized string
-  *
-  */
-
+ * cap_string - capitalizes all words of a string.
+ * @s: string.
+ * Return: string.
+ */
 char *cap_string(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		char j = s[i - 1];
-
-		if (j == ',' || j == ' ' || j == ';' || j == '.' || j == '!' ||
-		j == '?' || j == '"' || j == '(' || j == ')' || j == '{' ||
-		j == '}' || j == '\n' || j == '\t' && (s[i] >= 'a' && s[i] <= 'z'))
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] >= 'a' && s[i] <= 'z'))
 		{
 			s[i] = s[i] - 32;
-		} else if ((s[0] >= 97 && s[0] <= 122))
+		}
+		else if ((s[0] >= 97 && s[0] <= 122))
+		{
 			s[0] = s[0] - 32;
 		}
 		else
 		{
 			s[i] = s[i];
 		}
+		i++;
 	}
-
 	return (s);
 }
