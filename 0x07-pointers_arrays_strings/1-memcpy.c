5b4b5e7a4e18dd09d5280469061f596486bbfad8
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-03-07 00:36:48 +0100

    2-5

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _memcpy - a function that copies the first n bytes from area src to area dest
+ * _memcpy - a function that copies the first n bytes
+ * from area src to area dest
  * @dest: The string to be copied to
  * @n: Number of bytes in the string to be copied
  * @src: The string to be copied from
@@ -10,11 +11,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+    unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+    for (i = 0; i < n; i++)
+    {
+        dest[i] = src[i];
+    }
+    return (dest);
 }
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strchr - a function that locates a character in a string.
+ *
+ * @s: The string to be searched
+ * @c: The char to be searched for
+ *
+ * Return: Pointer to the first occurrence of char c and NULL
+ * if it isn't found
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+	char *p = &s[i];
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (p);
+		}
+
+		return (NULL);
+	}
+
+	return (NULL);
+}
