fd81c75e8aa209c9117c85414362cb30006abffd
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-03-07 10:24:10 +0100

    fix conflict

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
+/**
+ * _strspn - a function that gets the length of a prefix substring.
+ *
+ * @s: The string to be searched
+ * @accept: The char to be searched for
+ *
+ * Return: 0
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	return 0;
+	return (0);
 }
