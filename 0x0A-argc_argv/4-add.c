e9ded80db5b43ac0aa9a73e7597f7ee017abb14b
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2023-03-11 09:52:17 +0100

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
   * main - A function that adds positive numbers
@@ -13,14 +14,33 @@
   */
 
 int main(int argc, char *argv[])
-{
+{	
+	int s;
 	int i = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
+		return (1);
 	}
 
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(argv[i][0]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	
+	s = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		s = s + atoi(argv[i]);
+	}
+
+	printf("%i\n", s);
 	return (0);
 }
 
