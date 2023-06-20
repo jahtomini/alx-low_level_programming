e3508daf07ac41e84bb8f5e7b8f7c560404d8abb
jahtomini <93228906+jahtomini@users.noreply.github.com>
2023-06-20 01:32:14 +0000

    Structures, typedef - Fix Betty issues

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,17 +4,17 @@
 
 /**
  * init_dog - Create a dog struct.
- * 
+ *
  * @d: A pointer to the dog struct.
  * @name: The dog's name.
  * @age: The dog's age.
  * @owner: The dog's owner.
- * 
+ *
  * Return: Nothing.
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,10 +4,10 @@
 /**
  * free_dog - Free the memory allocated for a dog struct.
  * @d: A pointer to the dog struct.
- * 
+ *
  * Return: Nothing.
 */
 void free_dog(dog_t *d)
 {
-    free(d);
+	free(d);
 }
