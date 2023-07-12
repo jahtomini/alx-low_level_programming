b98de99c22dcb947fe39e433d3890e752705150a
jahtomini <93228906+jahtomini@users.noreply.github.com>
2023-07-12 11:10:20 +0000

    Add skeleton filees

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,10 @@ int _putchar(int i);
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
 unsigned int _pow(unsigned int base, unsigned int exp);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif
