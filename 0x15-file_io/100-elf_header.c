0454380fd13adbec533306784e9c471cc2ea6512
jahtomini <93228906+jahtomini@users.noreply.github.com>
2023-07-23 23:10:47 +0000

    File I/O - Task 4: fix header formatting

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -26,19 +26,26 @@ void print_elf_header(const Elf64_Ehdr *header)
 {
 	int i;
 
-	printf("Magic: ");
+	printf("ELF Header:\n");
+	printf("  Magic: ");
 	for (i = 0; i < EI_NIDENT; ++i)
 		printf("%02x ", header->e_ident[i]);
 
-	printf("\nClass: %s\n", (header->e_ident[EI_CLASS] == ELFCLASS32) ?
-	"ELF32" : "ELF64");
-	printf("Data: %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ?
-	"2's complement, little endian" : "2's complement, big endian");
-	printf("Version: %d\n", header->e_ident[EI_VERSION]);
-	printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
-	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("Type: 0x%x\n", header->e_type);
-	printf("Entry point address: 0x%lx\n", (unsigned long)header->e_entry);
+	printf("\n  Class:                            %s\n",
+	(header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("  Data:                            %s\n",
+	(header->e_ident[EI_DATA] == ELFDATA2LSB)
+	? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:                            %d\n",
+	header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %d\n",
+	header->e_ident[EI_OSABI]);
+	printf("  ABI Version:                            %d\n",
+	header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                            0x%x\n",
+	header->e_type);
+	printf("  Entry point address:                            0x%lx\n",
+	(unsigned long)header->e_entry);
 }
 
 /**
